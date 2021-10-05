0ce84233a22839c0ecde6cbbf34c9ba1ef9c8f15
dgop92 <inevaup@gmail.com>
2021-10-05 18:51:55 -0500

    feat: add help text in help dialog

--- src/interfaces/HelpDialog.java
+++ src/interfaces/HelpDialog.java
@@ -48,7 +48,7 @@ public class HelpDialog extends javax.swing.JDialog {
             .addGroup(headerLayout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(title_label)
-                .addContainerGap(334, Short.MAX_VALUE))
+                .addContainerGap(364, Short.MAX_VALUE))
         );
         headerLayout.setVerticalGroup(
             headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -61,7 +61,7 @@ public class HelpDialog extends javax.swing.JDialog {
         command_input_label.setBackground(new java.awt.Color(248, 249, 250));
         command_input_label.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         command_input_label.setForeground(new java.awt.Color(33, 33, 33));
-        command_input_label.setText("Ayuda lorem ipsum");
+        command_input_label.setText("<html>\n<b>Sección 1</b>: “Creación del grafo”<br><br>\n\n<b>Agregar Nodo</b>: Mientras se tenga seleccionado \n  se podrán pintar los nodos dentro del lienzo con \n  solo hacer click en la zona deseada. <br>\n\n<b>Agregar Arista</b>: Mientras se tenga seleccionado\n se podrán pintar las aristas. El proceso es tan \nsencillo como ingresar un valor numérico para \nel peso del arco o arista y elegir el par de nodos \ncorrespondiente.  <br>\n\n<b>Remover Nodo</b>: Mientras se tenga seleccionado\n se podrán eliminar vértices. Para \n eliminar solo basta con hacer click en \n un nodo ya existente. <br>\n<br>\n<b>Sección 2</b> “Algoritmos”<br><br>\n\n<b>Torre principa</b>l: Dado el nodo escrito en el campo de texto lo pondrá como si fuera la torre principal. \nSi la torre principal no es elegida, los algoritmos que la requieran tomarán una al azar. <br>\n\n<b>DFS y BFS</b>: Ejecuta los algoritmos involucrados. Los algoritmos empiezan desde la torre principal  <br>\n\n<b>Dijkstra</b>: Este botón ejecuta el algoritmo para conocer el camino \nmás corto desde la torre principal a una escrita en el campo de texto. <br>\n\n</html>");
         command_input_label.setVerticalAlignment(javax.swing.SwingConstants.TOP);
         command_input_label.setIconTextGap(12);
 
@@ -70,18 +70,18 @@ public class HelpDialog extends javax.swing.JDialog {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(header, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(command_input_label, javax.swing.GroupLayout.PREFERRED_SIZE, 367, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(command_input_label, javax.swing.GroupLayout.PREFERRED_SIZE, 397, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(header, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(command_input_label, javax.swing.GroupLayout.PREFERRED_SIZE, 446, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(19, Short.MAX_VALUE))
+                .addComponent(command_input_label, javax.swing.GroupLayout.DEFAULT_SIZE, 504, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -92,7 +92,7 @@ public class HelpDialog extends javax.swing.JDialog {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
