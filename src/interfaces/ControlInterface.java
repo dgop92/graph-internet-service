f3e46b09f106925f8d13dfeb7e1175885d384c56
dgop92 <inevaup@gmail.com>
2021-10-06 21:13:24 -0500

    fix: lorem ipsum error

--- src/interfaces/ControlInterface.java
+++ src/interfaces/ControlInterface.java
@@ -38,10 +38,6 @@ public class ControlInterface extends javax.swing.JFrame implements FeedBackObse
         updateButtonGroup(0, null);
         graphSketch.getGraphDrawer().setFeedBackObserver(this);
         algorithmManager = new AlgorithmManager(graphSketch.getGraphDrawer().getGraph());
-        
-        output_textarea.setText(
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        );
     }
 
     @Override
